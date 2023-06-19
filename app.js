057e2b718b35a754b5326740a17722c28f66af16
annasfe <anna_sfe@hotmail.com>
2023-06-19 19:14:31 +0300

    build commands

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("/", function(req, res, next) {
   res.send("Access the API at path /api");
@@ -24,7 +24,7 @@ app.use("/api", tripsRouter);
 
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/dist/index.html"));
 });
 
 // catch 404 and forward to error handler
